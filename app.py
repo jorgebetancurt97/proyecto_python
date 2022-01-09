094a2e936969b34c0ba4513db53ef8f51b6b918a
jorgebetancurt <jrgbetancurt@gmail.com>
2022-01-08 20:37:29 -0500

    se le inserta  establishConnectio

--- app.py
+++ app.py
@@ -6,68 +6,59 @@ from flask_mysqldb import MySQL
 from werkzeug.wrappers import request, response  
 import json
 from flask import Flask, request, Response, json
+import pymysql.cursors 
 
 #m conexion a mysq
 app = Flask(__name__)
-mysql = MySQL(app)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'contraseña'
-app.config['MYSQL_DB'] = 'service_db'
-
-
+                                
 def establishConnection():
-    connection=pymysql.connect(host='localhost',port=)
-    cursor=connection.cursor()
-    return (connection, cursor)
+   conn=pymysql.connect(host='localhost', user='root', password='0000', database='service_db', cursorclass=pymysql.cursors.DictCursor)
+   cursor=conn.cursor() 
+   
+   return (conn, cursor)
 
 ##listar cursos existentes*
 @app.route('/listar', methods=['GET'])
 def list():
-    conn,cur=establishConnection()
+    conn,cursor=establishConnection()
     try:
-        cur=mysql.connection.cursor()
-        cur.execute('SELECT * FROM registros') 
-        data=cur.fetchall()
-        listado = []
-        for fila in data:
-            lista = {'cedula':fila[0], 'nombres':fila[1], 'apellidos':fila[2], 'email':fila[3]}
-            listado.append(lista)
-        return jsonify({'lista de inscritos': listado, })    
-
-    except Exception as ex:
+        cursor.execute('SELECT * FROM registros') 
+        data=cursor.fetchall()
+        ##print (data)
+        ###return Response.json('data', data) 
+        return jsonify({'lista de inscritos': data,} )  
+            
+    
+    except Exception as e:
         return "No se pudo listar" 
     finally:
-        conn.close()
+       conn.close()
+    
         
 #listar un curso*
 @app.route('/listar/<cedula>', methods=['GET'])
 def leer_cedula(cedula):
     try:
-        cur=mysql.connection.cursor()
-        sql="SELECT * FROM registros WHERE cedula = '{0}' ".format(cedula)
-        cur.execute(sql)
-        data=cur.fetchone()
-        if data != None:
-            lista = {'cedula':data[0], 'nombres':data[1], 'apellidos':data[2], 'email':data[3]}
-            
-            
-            return jsonify({'mensaje' : lista })
-        else :
-            return jsonify({'mensaje': "curso no encontrado", }) 
+        conn,cursor=establishConnection()
+        cursor.execute("SELECT * FROM registros WHERE cedula = '{0}").format(cedula)
+        data=cursor.fetchone()
+        print (data)
+        return jsonify({'mensaje': data, }) 
 
-    except Exception as ex:
-        return jsonify({'mensaje': error, })  
+    except Exception as e:
+        return "usuario no encontrado" 
+    finally:
+       conn.close()  
 
 
 @app.route('/agregar',methods=['POST'])
 def agregar():
+    conn,cursor=establishConnection()
     try:
-        cur=mysql.connection.cursor()
         sql="""INSERT INTO registros (cedula, nombres, apellidos ,email )
         VALUES ('{0}','{1}','{2}','{3}') """.format(request.json['cedula'],request.json['nombres'] , request.json['apellidos'], request.json['email'] ) 
-        cur.execute(sql)
-        mysql.connection.commit()#confirmacion de insercion
+        cursor.execute(sql)
+        cursor.connection.commit()#confirmacion de insercion
         return jsonify({'mensaje': "agregado  correctamente "})  
     
         
@@ -77,10 +68,10 @@ def agregar():
 @app.route('/eliminar/<cedula>',methods=['DELETE'])
 def eliminar(cedula):
     try:
-        cur=mysql.connection.cursor()
+        conn,cursor=establishConnection()
         sql="DELETE FROM registros WHERE cedula = '{0}' ".format(cedula)
-        cur.execute(sql)
-        mysql.connection.commit()#confirmacion de insercion
+        cursor.execute(sql)
+        conn.connection.commit()#confirmacion de insercion
         return jsonify({'mensaje ': "eliminacion exitosa"})
     
     
@@ -91,12 +82,12 @@ def eliminar(cedula):
 @app.route('/actualizar/<cedula>',methods=['PUT'])
 def actualizar(cedula):
     try:
-        cur=mysql.connection.cursor()
+        conn,cursor=establishConnection()
         sql="""UPDATE registros SET nombres = '{0}' , apellidos = '{1}', email = '{2}'
         WHERE cedula '{3}' """.format(request.json['nombres'] , request.json['apellidos'], request.json['email'],cedula ) 
         
-        cur.execute(sql)
-        mysql.connection.commit()#confirmacion de insercion
+        cursor.execute(sql)
+        conn.connection.commit()#confirmacion de insercion
         return jsonify({'mensaje': "actualizacion de datos correctamente"})  
         
     
